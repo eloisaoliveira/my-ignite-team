480ff2a6d2c4f75f08e4ca1c744ea2d5d064590b
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2023-07-25 15:04:53 -0300

    add Loading component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
 
 import theme from './src/theme'
 
@@ -10,7 +11,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
